d7049657e1306ea8c37ac101ecffb2471227a40c
mahmoudShalby <mhmooud074@gmail.com>
2023-07-07 18:42:39 +0300

    changed tab system

--- src/routes/(auth)/auth/+page.svelte
+++ src/routes/(auth)/auth/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
     import Form from "$lib/components/Form.svelte"
-    import { fade } from "svelte/transition"
+    import { fade, scale } from "svelte/transition"
 
     const inputs = [
         { name: "email", type: "email" },
@@ -11,17 +11,15 @@
 </script>
 
 {#if tab === 'check first time'}
-    <div class="container w-[max(25vw,370px)]" transition:fade>
+    <div class="container w-[max(25vw,370px)]">
         <h1 class="text-3xl mb-4">Is this your first time here?</h1>
         <div class="grid grid-cols-2 gap-x-2">
-            <button class="emerald" on:click={() => setTimeout(() => (tab = 'auth'), 300)}>Yes</button>
+            <button class="emerald" on:click={() => tab = 'auth'}>Yes</button>
             <button>No</button>
         </div>
     </div>
 {:else if tab === 'auth'}
-    <div transition:fade>
-        <Form title="Enter your school" {inputs} action="">
-            <button type="submit" class="font-bold">Finished!</button>
-        </Form>
-    </div>
+    <Form title="Enter your school" {inputs} action="">
+        <button type="submit" class="font-bold">Finished!</button>
+    </Form>
 {/if}
